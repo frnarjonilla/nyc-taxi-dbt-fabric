111f235d001c6846f862aee5884da67b164da4fe
Francisco Arjonilla <frnarjonilla@gmail.com>
2026-04-28 22:28:40 +0200

    Committing 2 items from workspace 9c6a165d-0129-4ce4-97cf-94fc79edb6a0

---
+++ wh_taxis_dbt.Warehouse/dbo/Views/my_second_dbt_model.sql
@@ -0,0 +1,6 @@
+-- Auto Generated (Do not modify) 94D89976449D1C06EBAC95C7D083632AB9DCB4983A61DAEC9A68EF67B94CA26E
+create view [dbo].[my_second_dbt_model] as -- Use the `ref` function to select from other models
+
+select *
+from [wh_taxis_dbt].[dbo].[my_first_dbt_model]
+where id = 1;
