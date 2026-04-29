eaab9197ac76d0aa892a8194098197309196f020
frnarjonilla <frnarjonilla@gmail.com>
2026-04-29 22:50:19 +0200

    Convertido modelo a incremental para evitar duplicados

--- models/stg_taxis_nyc.sql
+++ models/stg_taxis_nyc.sql
@@ -1,7 +1,16 @@
-{{ config(materialized='incremental') }}
+{{
+  config(
+    materialized='incremental',
+    unique_key='id_unico'
+  )
+}}
 
 WITH trip_data AS (
     SELECT 
+        -- Creamos un ID único compuesto para evitar duplicados si se relanza el proceso
+        -- Esto concatena el ID del proveedor con la fecha exacta del viaje
+        CONCAT(VendorID, '_', tpep_pickup_datetime) AS id_unico,
+
         -- Identificadores y fechas
         CAST(VendorID AS INT) AS proveedor_id,
         CAST(tpep_pickup_datetime AS DATETIME2(6)) AS fecha_recogida,
@@ -16,7 +25,7 @@ WITH trip_data AS (
         CAST(PULocationID AS INT) AS zona_recogida_id,
         CAST(DOLocationID AS INT) AS zona_dejada_id,
         
-        -- Costes (usamos FLOAT por los decimales)
+        -- Costes
         CAST(payment_type AS INT) AS tipo_pago,
         CAST(fare_amount AS FLOAT) AS importe_tarifa,
         CAST(mta_tax AS FLOAT) AS impuesto_mta,
@@ -24,9 +33,18 @@ WITH trip_data AS (
         CAST(total_amount AS FLOAT) AS importe_total,
         CAST(congestion_surcharge AS FLOAT) AS recargo_congestion
 
-    FROM {{ source('staging_data', 'stg_taxis_raw') }}
+    FROM {{ source('staging_data', 'viajes_taxis_SL') }}
+
+    WHERE total_amount > 0 
+      AND trip_distance > 0 
+      AND passenger_count > 0
+
+    {% if is_incremental() %}
+      -- Este bloque solo se ejecuta cuando la tabla ya existe en Fabric
+      -- Filtramos para traer solo viajes cuya fecha sea posterior a la última guardada
+      AND tpep_pickup_datetime > (SELECT MAX(fecha_recogida) FROM {{ this }})
+    {% endif %}
 )
 
 SELECT * 
 FROM trip_data
-WHERE importe_total > 0 and distancia_viaje > 0 and numero_pasajeros > 0
