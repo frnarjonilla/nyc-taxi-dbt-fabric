0ad84803dbca88dd4bffdf3549a9e834e050c448
frnarjonilla <frnarjonilla@gmail.com>
2026-05-04 21:12:58 +0200

    fix: calendario usando cross join de numeros

--- models/dim_calendario.sql
+++ models/dim_calendario.sql
@@ -1,12 +1,20 @@
 {{ config(materialized='table') }}
 
-WITH RegistroFechas AS (
-    -- Definimos el inicio y el fin del calendario
-    SELECT CAST('2020-01-01' AS DATE) AS fecha
-    UNION ALL
-    SELECT DATEADD(day, 1, fecha)
-    FROM RegistroFechas
-    WHERE fecha < '2026-12-31'
+WITH 
+-- Generamos una base de números (10 x 10 x 10 x 10 = 10.000 filas posibles)
+t0 AS (SELECT 1 AS n UNION ALL SELECT 1),
+t1 AS (SELECT 1 AS n FROM t0 AS a CROSS JOIN t0 AS b),
+t2 AS (SELECT 1 AS n FROM t1 AS a CROSS JOIN t1 AS b),
+t3 AS (SELECT 1 AS n FROM t2 AS a CROSS JOIN t2 AS b),
+numeros AS (
+    SELECT ROW_NUMBER() OVER (ORDER BY (SELECT NULL)) - 1 AS id
+    FROM t3
+),
+fechas_base AS (
+    SELECT 
+        DATEADD(day, id, CAST('2020-01-01' AS DATE)) AS fecha
+    FROM numeros
+    WHERE id <= DATEDIFF(day, '2020-01-01', '2026-12-31')
 )
 SELECT
     fecha,
@@ -15,7 +23,6 @@ SELECT
     DAY(fecha) AS dia,
     DATEPART(QUARTER, fecha) AS trimestre,
     DATEPART(WEEK, fecha) AS semana,
-    -- Nombres en español
     CASE MONTH(fecha)
         WHEN 1 THEN 'Enero' WHEN 2 THEN 'Febrero' WHEN 3 THEN 'Marzo' 
         WHEN 4 THEN 'Abril' WHEN 5 THEN 'Mayo' WHEN 6 THEN 'Junio'
@@ -26,6 +33,4 @@ SELECT
         WHEN 1 THEN 'Domingo' WHEN 2 THEN 'Lunes' WHEN 3 THEN 'Martes' 
         WHEN 4 THEN 'Miércoles' WHEN 5 THEN 'Jueves' WHEN 6 THEN 'Viernes' WHEN 7 THEN 'Sábado'
     END AS nombre_dia
-FROM RegistroFechas
--- Importante: Fabric necesita esto para recursiones de más de 100 días
-OPTION (MAXRECURSION 0)
+FROM fechas_base
