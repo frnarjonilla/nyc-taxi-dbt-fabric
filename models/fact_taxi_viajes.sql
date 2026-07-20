1addd406e26b05f24d4d7374d76828a439a83774
Francisco Arjonilla <frnarjonilla@gmail.com>
2026-07-20 10:34:11 +0200

    Update fact_taxi_viajes.sql

--- models/fact_taxi_viajes.sql
+++ models/fact_taxi_viajes.sql
@@ -9,7 +9,7 @@ SELECT
     CAST(fecha_recogida AS DATE) AS fecha_id, -- Conecta con dim_calendario
     zona_recogida_id AS pickup_zona_id,       -- Conecta con dim_zones
     zona_dejada_id AS dropoff_zona_id,         -- Conecta con dim_zones
-    tipo_pago AS codigo_pago_id,               -- Conecta con cat_tipos_pago
+    CAST(tipo_pago AS INT) AS codigo_pago_id,               -- Conecta con cat_tipos_pago
     proveedor_id,
 
     -- MÉTRICAS
